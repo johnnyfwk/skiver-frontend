c7d77791b7e8520b37e832f0a8a49573a28c2a5f
johnnyfwk <johnnyfong@live.com>
2023-02-20 14:08:25 +0000

    Changed api url.

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const skiverApiBaseUrl = axios.create({
-    baseURL: 'http://localhost:9090/api'
-    // baseURL: 'https://skiver.onrender.com/api'
+    // baseURL: 'http://localhost:9090/api'
+    baseURL: 'https://skiver.onrender.com/api'
 })
 
 export function getUsers() {
